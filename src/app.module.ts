fa800e0cfb6be87dae6ae66c6bc7fab763285502
paulelyson <paulelyson.villaceran@gmail.com>
2026-02-23 17:27:47 +0800

    env and mongodb config on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { SchoolModule } from './school/school.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const DATABASE = process.env.DATABASE || 'mongodb://127.0.0.1:27017/asset_mgt_local';
 
 @Module({
-  imports: [UserModule, SchoolModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(DATABASE),
+    UserModule,
+    SchoolModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
