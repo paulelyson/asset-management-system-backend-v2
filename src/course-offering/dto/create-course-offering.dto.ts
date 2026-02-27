2d22e953dbd135fc664596883e191462dacd43cd
paulelyson <paulelyson.villaceran@gmail.com>
2026-02-27 22:30:02 +0800

    update on create course offering

--- src/course-offering/dto/create-course-offering.dto.ts
+++ src/course-offering/dto/create-course-offering.dto.ts
@@ -1,1 +1,37 @@
-export class CreateCourseOfferingDto {}
+import { Type } from 'class-transformer';
+import { ArrayNotEmpty, IsArray, IsEnum, IsMongoId, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { DayOfWeek } from 'src/common/enums/day-of-week.enum';
+
+export class CreateScheduleDto {
+  @IsEnum(DayOfWeek)
+  day: DayOfWeek;
+
+  @IsString()
+  @IsNotEmpty()
+  startTime: string;
+
+  @IsString()
+  @IsNotEmpty()
+  endTime: string;
+
+  @IsMongoId()
+  location: string;
+}
+
+export class CreateCourseOfferingDto {
+  @IsString()
+  @IsNotEmpty()
+  code: string;
+
+  @IsMongoId()
+  course: string;
+
+  @IsMongoId()
+  instructor: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => CreateScheduleDto)
+  schedule: CreateScheduleDto[];
+}
